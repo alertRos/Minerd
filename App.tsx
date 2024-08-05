1c78aee130521eafeb5a045a15cde4463ffbd6fe
Leny00 <lenyg675@gmail.com>
2024-08-04 20:29:13 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
-import { Image, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Horoscope from './components/Horoscope';
-import News from './components/News';
-import Visits from './components/Visits';
-import Weather from './components/Weather';
-import Profile from './components/Profile';
+import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingPage from './components/Onboarding';
+import OnboardingCreatePage from './components/Onboarding2';
+import OnboardingSigno from './components/Onboarding3';
+import OnboardingFinish from './components/Onboarding4';
+
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { View } from 'react-native';
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-// Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
+export type RootStackParamList = {
+  OnboardingPage: undefined;
+  OnboardingCreatePage: undefined;
+  OnboardingSigno: undefined;
+  OnboardingFinish: undefined;
+};
+
 export default function App() {
-  let [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     'alata-regular': require('./assets/fonts/Alata-Regular.ttf'),
   });
 
@@ -30,73 +36,26 @@ export default function App() {
     hideSplashScreen();
   }, [fontsLoaded]);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    // Optionally, you can return a loading view here if you want to show something else while loading
-    return null; // This will keep the splash screen visible
+    return null; // O muestra una pantalla de carga
   }
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator 
-        initialRouteName="Visits"
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconSource;
-
-            switch (route.name) {
-              case 'Visits':
-                iconSource = require('./assets/icons/visits.png');
-                break;
-              case 'Noticias':
-                iconSource = require('./assets/icons/news.png');
-                break;
-              case 'Clima':
-                iconSource = require('./assets/icons/weather.png');
-                break;
-              case 'Horóscopo':
-                iconSource = require('./assets/icons/horoscope.png');
-                break;
-              case 'Perfil':
-                iconSource = require('./assets/icons/profile.png');
-                break;
-              default:
-                iconSource = require('./assets/icons/visits.png');
-                break;
-            }
-
-            return (
-              <Image 
-                source={iconSource} 
-                style={{ width: size, height: size, tintColor: color }} 
-              />
-            );
-          },
-          tabBarLabel: ({ focused, color }) => (
-            <Text style={{ color, fontSize: 12, fontFamily: 'alata-regular', fontWeight: focused ? 'bold' : 'normal', bottom: 16 }}>
-              {route.name}
-            </Text>
-          ),
-          tabBarActiveTintColor: '#DC3545',
-          tabBarInactiveTintColor: 'white',
-          tabBarStyle: {
-            backgroundColor: '#0071BD', 
-            borderTopWidth: 0,
-            position: 'absolute', 
-            elevation: 0, 
-            borderTopRightRadius: 16,
-            borderTopLeftRadius: 16,
-            shadowOpacity: 0, 
-            height: 72, 
-          },
-        })}
-      >
-        <Tab.Screen name="Visits" component={Visits} />
-        <Tab.Screen name="Noticias" component={News} />
-        <Tab.Screen name="Clima" component={Weather} />
-        <Tab.Screen name="Horóscopo" component={Horoscope} />
-        <Tab.Screen name="Perfil" component={Profile} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="OnboardingPage" component={OnboardingPage} />
+          <Stack.Screen name="OnboardingCreatePage" component={OnboardingCreatePage} />
+          <Stack.Screen name="OnboardingSigno" component={OnboardingSigno} />
+          <Stack.Screen name="OnboardingFinish" component={OnboardingFinish} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
